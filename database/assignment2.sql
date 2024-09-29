65b5149cbc1b01595b6dc47e674e53ff34c61776
Dicmary <dicmaryzambrano@gmail.com>
2024-09-29 00:24:21 -0400

    created db files

---
+++ database/assignment2.sql
@@ -0,0 +1,59 @@
+-- #1 Insert
+INSERT INTO
+	PUBLIC.ACCOUNT (
+		ACCOUNT_FIRSTNAME,
+		ACCOUNT_LASTNAME,
+		ACCOUNT_EMAIL,
+		ACCOUNT_PASSWORD
+	)
+VALUES
+	(
+		'Tony',
+		'Stark',
+		'tony@starkent.com',
+		'Iam1ronM@n'
+	);
+
+-- #2 Update
+UPDATE PUBLIC.ACCOUNT
+SET
+	ACCOUNT_TYPE = 'Admin'
+WHERE
+	ACCOUNT_EMAIL = 'tony@starkent.com';
+
+-- #3 Delete
+DELETE FROM PUBLIC.ACCOUNT
+WHERE
+	ACCOUNT_EMAIL = 'tony@starkent.com';
+
+-- #4 Replace
+
+UPDATE PUBLIC.INVENTORY
+SET
+	INV_DESCRIPTION = REPLACE(
+		INV_DESCRIPTION,
+		'small interiors',
+		'a huge interior'
+	)
+WHERE
+	INV_MODEL = 'Hummer'
+	AND INV_MAKE = 'GM';
+
+-- #5 Inner join
+
+SELECT
+	INV_MAKE,
+	INV_MODEL,
+	CLASSIFICATION_NAME
+FROM
+	PUBLIC.INVENTORY
+	INNER JOIN PUBLIC.CLASSIFICATION ON INVENTORY.CLASSIFICATION_ID = CLASSIFICATION.CLASSIFICATION_ID
+WHERE
+	CLASSIFICATION_NAME = 'Sport';
+
+-- #6 Update and Replace Multiple
+
+UPDATE PUBLIC.INVENTORY
+SET
+	INV_IMAGE = REPLACE(INV_IMAGE, '/images/', '/images/vehicles/'),
+	INV_THUMBNAIL = REPLACE(INV_THUMBNAIL, '/images/', '/images/vehicles/');
